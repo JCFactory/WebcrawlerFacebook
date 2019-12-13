5c18892113e160dc5df99cfff550dcbc98705800
Jacqueline Franßen <jacqueline.franssen@ibm.com>
2019-12-13 19:07:16 +0100

    added percentage

--- flask-app/fapp/fapppack/reporting/facebook_api.py
+++ flask-app/fapp/fapppack/reporting/facebook_api.py
@@ -51,5 +51,48 @@ class FacebookApi:
         return df_comments
 
 
+    def analyze(self):
+        negcount = 0
+        poscount = 0
+        negseries = 0
+
+        reportneg = False
+
+        df_comments = self.callFacebookApi()
+
+        for el in df_comments['comments']:
+            el = tokenizer.texts_to_sequences(el)
+            flat_list = []
+            for sublist in el:
+                for item in sublist:
+                    flat_list.append(item)
+            flat_list = [flat_list]
+            el = pad_sequences(flat_list, padding='post', maxlen=100)
+
+            predictvalue = self.model.predict(el)
+
+            if (predictvalue <= 0.5):
+                negcount += 1
+                negseries += 1
+                sentiment = 'negativ'
+                df_comments.insert(2, "Sentiment", sentiment, True)
+
+            elif (predictvalue > 0.5):
+                poscount += 1
+                negseries = 0
+                sentiment = 'positiv'
+                df_comments.insert(2, "Sentiment", sentiment, True)
+
+            if (negseries == 5 and reportneg == False):
+                reportneg = True
+
+        return {'df_comments': df_comments, 'negative': negcount, 'positive': poscount, 'total': negcount + poscount,
+                'reportnegative': reportneg,
+                'negativepercent': negcount / (negcount + poscount) * 100,
+                'positivepercent': poscount / (negcount + poscount) * 100}
+
+
+
+
 
 
