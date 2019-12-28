bc8f9398e21b63cd8167cb69bfa081d5502fb95b
Marvin Wobbe <marvin.wobbe@icloud.com>
2019-12-29 00:47:13 +0100

    Import CNN Dataframe Anpassung Datetime Timezones

--- flask-app/fapp/fapppack/reporting/report.py
+++ flask-app/fapp/fapppack/reporting/report.py
@@ -4,8 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 import datetime as dt
+import pytz
+import random
 from fpdf import FPDF
-from datetime import date, datetime, timedelta
+from datetime import date, datetime, timedelta, timezone
 from pandas.plotting import register_matplotlib_converters
 
 
@@ -35,9 +37,9 @@ class Report:
             ## Positive Percent: {}"""
 
         print(self.report_data["df_comments"]["time"])
-        df123 = self.report_data["df_comments"]
+        dfcsv = self.report_data["df_comments"]
         csvfile = self.static_folder + "/export_dataframe.csv"
-        df123.to_csv(csvfile, index = None, sep = '|')
+        dfcsv.to_csv(csvfile, index = None, sep = '|')
         if len(self.report_data['newcomments']) > 0:
             summary += "\n\nNew Comments are:"
             for comment in self.report_data['newcomments']:
@@ -68,35 +70,53 @@ class Report:
     def dataimport(self):
         # Create date parameter
         df = pd.DataFrame()
+        df_demo = pd.DataFrame()
 
         # Read demo data into dataframe
         fakedata = np.loadtxt('FakeData.csv', delimiter=';')
 
-        df['id'] = fakedata[:, 0]
-        df['date'] = fakedata[:, 1]
-        df['month'] = fakedata[:, 2]
-        df['kw'] = fakedata[:, 3]
-        df['weekday'] = fakedata[:, 4]
-        df['day'] = fakedata[:, 7]
-        df['hour'] = fakedata[:, 8]
-        df['minute'] = fakedata[:, 9]
-        df['weekhour'] = fakedata[:, 10]
-        df['sentiment'] = fakedata[:, 11]
-        df['real_date'] = pd.TimedeltaIndex(df['date'], unit='d') + dt.datetime(1900, 1, 1)
-        df['real_time'] = pd.to_timedelta(df["hour"], unit='h')
-        df['real_datetime'] = pd.to_datetime(df['real_date'] + df['real_time'])
-        # return (minus1, minus7, minus31, minus365)
-        # return (count_minus1_neg, count_minus7_neg, count_minus31_neg, count_minus365_neg)
-        # return (count_minus1_pos, count_minus7_pos, count_minus31_pos, count_minus365_neg)
-        return df
+        df_demo['id'] = fakedata[:, 0]
+        df_demo['date'] = fakedata[:, 1]
+        df_demo['month'] = fakedata[:, 2]
+        df_demo['kw'] = fakedata[:, 3]
+        df_demo['weekday'] = fakedata[:, 4]
+        df_demo['day'] = fakedata[:, 7]
+        df_demo['hour'] = fakedata[:, 8]
+        df_demo['minute'] = fakedata[:, 9]
+        df_demo['weekhour'] = fakedata[:, 10]
+        df_demo['sentiment'] = fakedata[:, 11]
+        df_demo['real_date'] = pd.TimedeltaIndex(df_demo['date'], unit='d') + dt.datetime(1900, 1, 1)
+        df_demo['real_time'] = pd.to_timedelta(df_demo["hour"], unit='h')
+        df_demo['real_datetime'] = pd.to_datetime(df_demo['real_date'] + df_demo['real_time'])
 
-        # Define and use a simple function to label the plot in axes coordinates
+        cnndata = self.report_data["df_comments"]
+        # Read CNN Data
+        df_cnn = cnndata
+        df_cnn.rename(columns={"comment": "comment", "time": "real_datetime", "Sentiment": "sentiment"}, inplace=True)
+        df_cnn.drop(df_cnn.columns.difference(['real_datetime', 'sentiment']), 1, inplace=True)
+        df_cnn.loc[df_cnn.sentiment == 'negativ', 'sentiment'] = 2
+        df_cnn.loc[df_cnn.sentiment == 'positiv', 'sentiment'] = 1
+        df_cnn['real_datetime'] = pd.to_datetime(df_cnn['real_datetime'])
+        df_cnn['id'] = random.randint(51000, 60000)
+        df_cnn['date'] = pd.to_datetime(df_cnn['real_datetime']).dt.date
+        df_cnn['month'] = pd.DatetimeIndex(df_cnn['real_datetime']).month
+        df_cnn['kw'] = df_cnn['real_datetime'].dt.week
+        df_cnn['weekday'] = pd.DatetimeIndex(df_cnn['real_datetime']).weekday
+        df_cnn['day'] = pd.to_datetime(df_cnn['real_datetime']).dt.day
+        df_cnn['hour'] = pd.to_datetime(df_cnn['real_datetime']).dt.hour
+        df_cnn['minute'] = pd.to_datetime(df_cnn['real_datetime']).dt.minute
+        df_cnn['weekhour'] = (df_cnn['weekday'].astype(float) - 1) * 24 + df_cnn['hour'].astype(float)
+        df_cnn['real_date'] = pd.to_datetime(df_cnn['real_datetime']).dt.date
+        df_cnn['real_time'] = pd.to_timedelta(df_cnn["hour"], unit='h')
+        df = pd.concat([df_demo, df_cnn], ignore_index=True, sort=False)
+        return df
 
     def weekplots(self, df):
         # Prepares the dataframes for the weekplots
+        now = datetime.now()
         df_week2 = df.copy()
         df_week2.drop(df_week2.columns.difference(['sentiment', 'weekhour', 'kw', 'real_datetime']), 1, inplace=True)
-        kw_now = datetime.now().date().isocalendar()[1]
+        kw_now = datetime.date(now).isocalendar()[1]
         df_week2.drop(df_week2[df_week2['kw'] < kw_now - 8].index, inplace=True)
         df_week2.drop(df_week2[df_week2['kw'] > kw_now - 1].index, inplace=True)
         df_week2["kw"] = df_week2["kw"].astype(int)
@@ -109,7 +129,6 @@ class Report:
         sns.set(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
 
         pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
-        print(df_week1)
         weekplot1 = sns.FacetGrid(df_week1, row="kw", hue="kw", aspect=10, height=0.75, palette=pal)
 
         # Draw the densities in a few steps
@@ -117,7 +136,13 @@ class Report:
         weekplot1.map(sns.kdeplot, "weekhour", clip_on=False, color="w", lw=2, bw=.2)
         weekplot1.map(plt.axhline, y=0, lw=2, clip_on=False)
 
-        weekplot1.map(self.label, "weekhour")
+        # Define and use a simple function to label the plot in axes coordinates
+        def label(weekhour, color, label):
+            ax = plt.gca()
+            ax.text(0, .2, label, fontweight="bold", color=color,
+                    ha="left", va="center", transform=ax.transAxes)
+
+        weekplot1.map(label, "weekhour")
 
         # Set the subplots to overlap
         weekplot1.fig.subplots_adjust(hspace=-0.5)
@@ -129,7 +154,7 @@ class Report:
         weekplot1.axes[7, 0].set_xlabel('Wochenstunde 7x24')
         weekplot1.despine(bottom=True, left=True)
 
-        weekplot1.savefig(self.static_folder + "/WeekPlot1.png")
+        weekplot1.savefig("WeekPlot1.png", bbox_inches='tight')
 
         # Creates the weekplot 2
         weekplot2 = sns.FacetGrid(df_week2, row="kw", hue="kw", aspect=10, height=0.75, palette=pal)
@@ -139,7 +164,7 @@ class Report:
         weekplot2.map(sns.kdeplot, "weekhour", clip_on=False, color="w", lw=2, bw=.2)
         weekplot2.map(plt.axhline, y=0, lw=2, clip_on=False)
 
-        weekplot2.map(self.label, "weekhour")
+        weekplot2.map(label, "weekhour")
 
         # Set the subplots to overlap
         weekplot2.fig.subplots_adjust(hspace=-0.5)
@@ -151,14 +176,29 @@ class Report:
         weekplot2.axes[7, 0].set_xlabel('Wochenstunde 7x24')
         weekplot2.despine(bottom=True, left=True)
 
-        weekplot2.savefig(self.static_folder + "/WeekPlot2.png")
+        weekplot2.savefig("WeekPlot2.png", bbox_inches='tight')
 
     def dayplot(self, df):
         # Prepares the dataframe for the dayplot
+        utc = pytz.utc
+        now = datetime.now(timezone.utc)
         df_day = df.copy()
         df_day.drop(df_day.columns.difference(['sentiment', 'real_datetime']), 1, inplace=True)
-        minus2 = datetime.today() - timedelta(days=2)
-        now = datetime.now()
+        daysvar = 1
+        count = 0
+
+        # Check for records and adjusting timeframe accordingly
+        while count < 2:
+            daysvar = daysvar + 1
+            minus2 = datetime.now(timezone.utc) - timedelta(days=daysvar)
+            df_copy = df_day.copy()
+            df_copy['real_datetime'] = pd.to_datetime(df_copy['real_datetime'], utc=True)
+            df_copy.drop(df_copy[df_copy['real_datetime'] < minus2].index, inplace=True)
+            df_copy.drop(df_copy[df_copy['real_datetime'] > now].index, inplace=True)
+            count = len(df_copy.index)
+
+        minus2 = datetime.now(timezone.utc) - timedelta(days=daysvar)
+        df_day['real_datetime'] = pd.to_datetime(df_day['real_datetime'], utc=True)
         df_day.drop(df_day[df_day['real_datetime'] < minus2].index, inplace=True)
         df_day.drop(df_day[df_day['real_datetime'] > now].index, inplace=True)
         df_day_sum = df_day.groupby(['real_datetime', 'sentiment']).size().unstack(fill_value=0)
@@ -169,38 +209,39 @@ class Report:
         sns.set(style="whitegrid")
         colors = ["#01355B", "#FFC000"]
         dayplot = sns.lineplot(data=df_day_sum, palette=(colors), linewidth=2.5)
-        dayplot.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
+        dayplot.xaxis.set_major_formatter(md.DateFormatter('%d.%m\n%H:%M'))
         dayplot.set_ylabel('Anzahl')
-        dayplot.set_xlabel('Uhrzeit')
+        dayplot.set_xlabel('Datum/Uhrzeit')
         dayplotfigure = dayplot.get_figure()
-        dayplotfigure.savefig(self.static_folder + "/DayPlot.png")
+        dayplotfigure.savefig('DayPlot.png', bbox_inches='tight')
 
     def pdfgenerator(self, df):
         # Todays date
         today = date.today()
         strtoday = today.strftime("%d.%m.%Y")
+        df['real_datetime'] = pd.to_datetime(df['real_datetime'], utc=True)
 
         # variables
-        minus1 = datetime.today() - timedelta(days=1)
-        minus7 = datetime.today() - timedelta(days=7)
-        minus31 = datetime.today() - timedelta(days=31)
-        minus365 = datetime.today() - timedelta(days=365)
+        minus1 = datetime.now(timezone.utc) - timedelta(days=1)
+        minus7 = datetime.now(timezone.utc) - timedelta(days=7)
+        minus31 = datetime.now(timezone.utc) - timedelta(days=31)
+        minus365 = datetime.now(timezone.utc) - timedelta(days=365)
 
         # Counting different timeframes for the reporting tiles
-        array_minus1_neg = df[(df.real_date > minus1) & (df.sentiment == 2)].count()
-        array_minus7_neg = df[(df.real_date > minus7) & (df.sentiment == 2)].count()
-        array_minus31_neg = df[(df.real_date > minus31) & (df.sentiment == 2)].count()
-        array_minus365_neg = df[(df.real_date > minus365) & (df.sentiment == 2)].count()
+        array_minus1_neg = df[(df.real_datetime > minus1) & (df.sentiment == 2)].count()
+        array_minus7_neg = df[(df.real_datetime > minus7) & (df.sentiment == 2)].count()
+        array_minus31_neg = df[(df.real_datetime > minus31) & (df.sentiment == 2)].count()
+        array_minus365_neg = df[(df.real_datetime > minus365) & (df.sentiment == 2)].count()
 
         count_minus1_neg = array_minus1_neg[0]
         count_minus7_neg = array_minus7_neg[0]
         count_minus31_neg = array_minus31_neg[0]
         count_minus365_neg = array_minus365_neg[0]
 
-        array_minus1_ges = df[(df.real_date > minus1)].count()
-        array_minus7_ges = df[(df.real_date > minus7)].count()
-        array_minus31_ges = df[(df.real_date > minus31)].count()
-        array_minus365_ges = df[(df.real_date > minus365)].count()
+        array_minus1_ges = df[(df.real_datetime > minus1)].count()
+        array_minus7_ges = df[(df.real_datetime > minus7)].count()
+        array_minus31_ges = df[(df.real_datetime > minus31)].count()
+        array_minus365_ges = df[(df.real_datetime > minus365)].count()
 
         count_minus1_ges = array_minus1_ges[0]
         count_minus7_ges = array_minus7_ges[0]
@@ -242,7 +283,7 @@ class Report:
         pdf.set_font('arial', '', 14)
 
         # Adding Plots
-        pdf.set_xy(108, 111)
+        pdf.set_xy(108, 113)
         pdf.image(self.static_folder + '/DayPlot.png', x=None, y=None, w=92, h=0, type='', link='')
         pdf.set_xy(15, 196)
         pdf.image(self.static_folder + '/WeekPlot1.png', x=None, y=None, w=85, h=0, type='', link='')
